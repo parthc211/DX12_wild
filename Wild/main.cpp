4f903a714f2e18f755e50cd757f1a07761a0f837
Parth Contractor <parth.contractor94@gmail.com>
2019-04-14 02:15:18 -0400

    Initialize D3D 12 (in progress)

--- Wild/main.cpp
+++ Wild/main.cpp
@@ -1,5 +1,7 @@
 #include "stdafx.h"
 
+using namespace DirectX; // we will be using the directxmath library
+
 int WINAPI WinMain(HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine,
@@ -13,8 +15,23 @@ int WINAPI WinMain(HINSTANCE hInstance,
 		return 0;
 	}
 
+	// initialize direct3d
+	if (!InitD3D())
+	{
+		MessageBox(0, L"Failed to initialize direct3d 12",
+			L"Error", MB_OK);
+		Cleanup();
+		return 1;
+	}
+
 	// start the main loop
 	mainloop();
+
+	// we want to wait for the gpu to finish executing the command list before we start releasing everything
+	WaitForPreviousFrame();
+
+	// close the fence event
+	CloseHandle(fenceEvent);
 
 	return 0;
 }
@@ -133,4 +150,64 @@ LRESULT CALLBACK WndProc(HWND hwnd,
 		msg,
 		wParam,
 		lParam);
+}
+
+bool InitD3D()
+{
+	HRESULT hr;
+
+	// -- Create the device -- //
+
+	IDXGIFactory4* dxgiFactory;
+	hr = CreateDXGIFactory1(IID_PPV_ARGS(&dxgiFactory));
+	if (FAILED(hr))
+	{
+		return false;
+	}
+
+	IDXGIAdapter1* adapter; // adapters are the graphics card (this includes the embedded graphics on the motherboard)
+
+	int adapterIndex = 0; // we'll start looking for directx 12 compatible graphics device starting at index 0
+
+	bool adapterFound = false; // set this to true when a good one was found
+
+	// find first hardware gpu that supports d3d 12
+	while (dxgiFactory->EnumAdapters1(adapterIndex, &adapter) != DXGI_ERROR_NOT_FOUND)
+	{
+		DXGI_ADAPTER_DESC1 desc;
+		adapter->GetDesc1(&desc);
+
+		if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)
+		{
+			// we dont want a software device
+			adapterIndex++;
+			continue;
+		}
+
+		// we want a device that is compatible with direct3d 12 (feature level 11 or higher)
+		hr = D3D12CreateDevice(adapter, D3D_FEATURE_LEVEL_11_0, _uuidof(ID3D12Device), nullptr);
+		if (SUCCEEDED(hr))
+		{
+			adapterFound = true;
+			break;
+		}
+
+		adapterIndex++;
+	}
+
+	if (!adapterFound)
+	{
+		return false;
+	}
+
+	// Create the device
+	hr = D3D12CreateDevice(
+		adapter,
+		D3D_FEATURE_LEVEL_11_0,
+		IID_PPV_ARGS(&device)
+	);
+	if (FAILED(hr))
+	{
+		return false;
+	}
 }
