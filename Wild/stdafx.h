c1501ae37368949c27095acf1536adefdb8a418a
Parth Contractor <parth.contractor94@gmail.com>
2019-04-14 00:42:39 -0400

    Created Window

--- Wild/stdafx.h
+++ Wild/stdafx.h
@@ -10,3 +10,34 @@
 #include <d3dcompiler.h>
 #include <DirectXMath.h>
 #include "d3dx12.h"
+
+// Handle to the window
+HWND hwnd = NULL;
+
+// name of the window (not the title)
+LPCTSTR WindowName = L"WildEngineApp";
+
+// title of the window
+LPCTSTR WindowTitle = L"Wild Window";
+
+// width and height of the window
+int Width = 800;
+int Height = 600;
+
+// is window full screen?
+bool FullScreen = false;
+
+// create a window
+bool InitializeWindow(HINSTANCE hInstance,
+	int ShowWnd,
+	int width, int height,
+	bool fullscreen);
+
+// main application loop
+void mainloop();
+
+// callback function for windows messages
+LRESULT CALLBACK WndProc(HWND hWnd,
+	UINT msg,
+	WPARAM wParam,
+	LPARAM lParam);
